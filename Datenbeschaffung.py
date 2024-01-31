1d755bb7d71fe663f1237170b46963ad87f1a2d5
DESKTOP-0AS7UAT\erikm <erik.marr@mailbox.tu-dresden.de>
2024-01-31 12:00:48 +0100

    For Schleife mit apply() ersetzt um Effizienz zu steigern

--- Datenbeschaffung.py
+++ Datenbeschaffung.py
@@ -33,22 +33,32 @@ data = data.reset_index()
 info_array = pd.DataFrame(columns=['Zeilenname', 'Spaltenname', 'Wert'])
 
 
-# Schleife durch die Zeilen des DataFrames
-print(data)
-for index, row in data.iterrows():
-    for column in data.columns:
-        zeilenname = row[data.columns[0]]
-        spaltenname = column
-        wert = data.at[index, column]
+# # Schleife durch die Zeilen des DataFrames
+# print(data)
+# for index, row in data.iterrows():
+#     for column in data.columns:
+#         zeilenname = row[data.columns[0]]
+#         spaltenname = column
+#         wert = data.at[index, column]
+#
+#         # Erstellen eines temporären DataFrames für jede Zelle
+#         temp_df = pd.DataFrame({'Zeilenname': [zeilenname], 'Spaltenname': [spaltenname], 'Wert': [wert]})
+#         # Verwenden Sie concat, um den temporären DataFrame zum info_array hinzuzufügen
+#         info_array = pd.concat([info_array, temp_df], ignore_index=True)
 
-        # Erstellen eines temporären DataFrames für jede Zelle
-        temp_df = pd.DataFrame({'Zeilenname': [zeilenname], 'Spaltenname': [spaltenname], 'Wert': [wert]})
-        # Verwenden Sie concat, um den temporären DataFrame zum info_array hinzuzufügen
-        info_array = pd.concat([info_array, temp_df], ignore_index=True)
+def process_cell(row):
+    row_df = pd.DataFrame({
+        'Zeilenname': row.name,
+        'Spaltenname': row.index,
+        'Wert': row.values
+    })
+    return row_df
 
+# Anwenden der Funktion auf jede Zeile und Konkatenation der Ergebnisse
+info_array = pd.concat([process_cell(data.iloc[i]) for i in range(len(data))]).reset_index(drop=True)
 
 # Angenommener Dateiname für die CSV-Datei
-csv_dateiname = Pfad + '/exportierte_data.txt'
+csv_dateiname = Pfad + '/exportierte_data1.txt'
 # Exportiere den DataFrame in eine CSV-Datei
 info_array.to_csv(csv_dateiname, index=True)
 
