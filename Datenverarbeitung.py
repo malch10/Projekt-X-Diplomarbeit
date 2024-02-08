678abc1e0627f3f137c80f06f0f22b2a77059b18
s2008758 <erik.marr@mailbox.tu-dresden.de>
2024-02-08 12:16:43 +0100

    Tippfehler Korrektur

--- Datenverarbeitung.py
+++ Datenverarbeitung.py
@@ -5,7 +5,7 @@ import time
 start_time = time.time()
 Pfad = 'C:/Users/erikm/Desktop/Diplomarbeit Erik Marr/Daten/I7000_F9000'
 
-valu_dateien = glob.glob(Pfad + '/*_exportierte_data2.pkl')
+valu_dateien = glob.glob(Pfad + '/*_exportierte_data.pkl')
 
 for datei in valu_dateien:
     data = pd.read_pickle(datei)
@@ -13,7 +13,7 @@ for datei in valu_dateien:
 
     # Temperaturen entfernen, welche kleiner als Zimmertemperatur sind
     filtered_values = data.loc[data['Temperatur'] < 290, 'Temperatur']
-    pkl_dateiname = datei.replace('_exportierte_data2.pkl', '_finish_dat2.pkl')
+    pkl_dateiname = datei.replace('_exportierte_data.pkl', '_finish_data.pkl')
     data.to_pickle(pkl_dateiname)
     data = data.drop(filtered_values.index)
     data.reset_index(drop=True, inplace=True)
