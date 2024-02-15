34923c3ffa1c9a4fe52880b2897ac6d092016e1c
s2008758 <erik.marr@mailbox.tu-dresden.de>
2024-02-15 21:56:20 +0100

    Entfernung von Radius Größer 0,0025

--- Datenverarbeitung.py
+++ Datenverarbeitung.py
@@ -5,21 +5,30 @@ import time
 start_time = time.time()
 Pfad = 'C:/Users/erikm/Desktop/Diplomarbeit Erik Marr/Daten/I7000_F9000'
 
+#valu_dateien = glob.glob(Pfad + '/*_exportierte_data_D1.pkl')
+#valu_dateien = glob.glob(Pfad + '/*_exportierte_data_D2.pkl')
+#valu_dateien = glob.glob(Pfad + '/*_exportierte_data_D3.pkl')
 valu_dateien = glob.glob(Pfad + '/*_exportierte_data_D4.pkl')
+
 
 for datei in valu_dateien:
     data = pd.read_pickle(datei)
     data.columns = ['X-Koordinate','Y-Koordinate','Zeitpunkt','Strom','Kraft','Temperatur']
 
-    # Temperaturen entfernen, welche kleiner als Zimmertemperatur sind
-    filtered_values = data.loc[data['Temperatur'] < 290, 'Temperatur']
-    data = data.drop(filtered_values.index)
+
+
+    filtered_Radius = data.loc[data['X-Koordinate'] > 0.0025, 'X-Koordinate']
+
+    data = data.drop(filtered_Radius.index)
     data.reset_index(drop=True, inplace=True)
     print(data)
-    print(f' Filtered: {filtered_values.count()}')
+    print(f' Filtered: {filtered_Radius.count()}')
+    #pkl_dateiname = datei.replace('_exportierte_data_D1.pkl', '_finish_data_D1.pkl')
+    #pkl_dateiname = datei.replace('_exportierte_data_D2.pkl', '_finish_data_D2.pkl')
+    #pkl_dateiname = datei.replace('_exportierte_data_D3.pkl', '_finish_data_D3.pkl')
     pkl_dateiname = datei.replace('_exportierte_data_D4.pkl', '_finish_data_D4.pkl')
-    data.to_pickle(pkl_dateiname)
 
+    data.to_pickle(pkl_dateiname)
 
 end_time = time.time()
 
