3a3640e2b832aaab9ed7d75ae5cdc883cfb84b67
s2008758 <erik.marr@mailbox.tu-dresden.de>
2024-03-20 15:30:41 +0100

    Erstellung NeuroNetz_D4_t_I_F_1

--- Dateien_einlesen.py
+++ Dateien_einlesen.py
@@ -2,6 +2,6 @@ import pandas as pd
 import pickle
 
 
-geladener_df = pd.read_pickle('C:/Users/erikm/Desktop/Diplomarbeit Erik Marr/Daten/I7000_F9000/TPath_8_finish_data.pkl')
+geladener_df = pd.read_pickle('C:/Users/erikm/Desktop/Diplomarbeit Erik Marr/Daten/Finish/Finish_D4_t_I_F_PKL/Finish_ALL_D4_t_I_F_PKL')
 
 print(geladener_df)
--- Kombinieren_D4_t_I_F.py
+++ Kombinieren_D4_t_I_F.py
@@ -1,25 +1,34 @@
 import os
 import pickle
 
+import os
+import pandas as pd
 
-def combine_pkl_files(folder_path, output_file_path):
-    # Liste aller .pkl-Dateien im angegebenen Ordner
+
+def combine_pandas_pkl_files(folder_path, output_file_path):
     pkl_files = [file for file in os.listdir(folder_path) if file.endswith('.pkl')]
 
-    combined_data = []
+    combined_df = pd.DataFrame()
+    first_file = True
     for file_name in pkl_files:
         file_path = os.path.join(folder_path, file_name)
-        with open(file_path, 'rb') as file:
-            # Lade die Daten aus der .pkl-Datei
-            data = pickle.load(file)
-            combined_data.append(data)
+        df = pd.read_pickle(file_path)
 
-    # Schreibe die kombinierten Daten in eine neue .pkl-Datei
-    with open(output_file_path, 'wb') as output_file:
-        pickle.dump(combined_data, output_file)
+        if first_file:
+            # Speichere die Spaltennamen der ersten Datei
+            column_names = df.columns
+            first_file = False
+        else:
+            # Setze die Spaltennamen in allen folgenden DataFrames auf die der ersten Datei
+            df = df.reindex(columns=column_names)
+
+        combined_df = pd.concat([combined_df, df], ignore_index=True)
+
+    # Speichere den kombinierten DataFrame als .pkl-Datei
+    combined_df.to_pickle(output_file_path)
 
 
 # Beispielaufruf der Funktion
 folder_path = 'C:/Users/erikm/Desktop/Diplomarbeit Erik Marr/Daten/Finish/Finish_D4_t_I_F_PKL'  # Ersetzen Sie dies mit dem Pfad zu Ihrem Ordner
-output_file_path = 'C:/Users/erikm/Desktop/Diplomarbeit Erik Marr/Daten/Finish/Finish_ALL_D4_t_I_F_PKL'  # Name der kombinierten .pkl-Datei
-combine_pkl_files(folder_path, output_file_path)
+output_file_path = 'C:/Users/erikm/Desktop/Diplomarbeit Erik Marr/Daten/Finish/Finish_ALL_D4_t_I_F_PKL.pkl'  # Name der kombinierten .pkl-Datei
+combine_pandas_pkl_files(folder_path, output_file_path)
